d3860b8297d51f4ac32d21568bfac75f0b1de065
Piers <piersmoly@gmail.com>
2024-01-04 09:14:49 +0000

    Create loader.py
    - Create load_image function

---
+++ backend/app/image_processing/loader.py
@@ -0,0 +1,30 @@
+# This is the loader module for loading in sudoku images belonging to the image_processing package 
+import cv2
+
+
+def load_image(file_name: str):
+    """
+    Load an image from a specified file.
+
+    Parameters:
+    file_name (str): The path to the image file.
+
+    Returns:
+    np.ndarray: The loaded image in grayscale format, or None if loading fails.
+
+    Raises:
+    FileNotFoundError: If the file does not exist.
+    """
+    # Check if file exists
+    if not os.path.exists(file_name):
+        raise FileNotFoundError(f"The file {file_name} does not exist.")
+
+    # Load the image in grayscale
+    original_image = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
+
+    # Check if the image was loaded successfully
+    if original_image is None:
+        print(f"Failed to load the image from {file_name}.")
+        return None
+
+    return original_image
