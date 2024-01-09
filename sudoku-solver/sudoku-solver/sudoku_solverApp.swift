8d38f681a574fb244c689d633400f894746c0144
Piers <piersmoly@gmail.com>
2024-01-09 15:47:37 +0000

    Create XCode project for frontend

---
+++ sudoku-solver/sudoku-solver/sudoku_solverApp.swift
@@ -0,0 +1,17 @@
+//
+//  sudoku_solverApp.swift
+//  sudoku-solver
+//
+//  Created by Molyneux, Piers on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct sudoku_solverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
